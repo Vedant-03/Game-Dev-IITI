9c32493e00b0e733b63a570e6a3399c4fa88a6f9
FRANKENSTEIN0211 <priyanshugrao369@gmail.com>
2022-02-10 00:46:36 +0530

    bot script

---
+++ TAG/Assets/bot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class bot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    public NavMeshAgent agent;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+}
