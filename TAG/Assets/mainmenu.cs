4bb9c83d89462f607c64f4d08daa8bcb2a0e6b05
FRANKENSTEIN0211 <priyanshugrao369@gmail.com>
2022-02-15 19:59:28 +0530

    main menu complete

---
+++ TAG/Assets/mainmenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+    public void GoToSettingsMenu()
+    {
+        SceneManager.LoadScene("settingsmenu");
+    }
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("mainmenu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
