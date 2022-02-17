3bf0dc40700647128aa3e423af7bf94f1b0fe48c
FRANKENSTEIN0211 <priyanshugrao369@gmail.com>
2022-02-17 16:58:36 +0530

    menu complete

---
+++ TAG/Assets/settingsmenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class settingsmenu : MonoBehaviour
+{
+  public AudioMixer audiomixer;
+  public void setvolume(float volume)
+  {
+    audiomixer.SetFloat("volume",volume);
+   Debug.Log(volume);
+  }
+}
